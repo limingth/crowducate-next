61aa3744765b0767c0fe697dd51b168c8765a76d
Liu Jiyang <2411mail@gmail.com>
2015-05-05 16:55:29 +0800

    change course page styles

--- both/router/meta.js
+++ both/router/meta.js
@@ -2,7 +2,8 @@ Meteor.startup(function() {
   if(Meteor.isClient) {
     Meta.config({
       options: {
-        // Meteor.settings[Meteor.settings.environment].public.meta.title
+        //Meteor.settings[Meteor.settings.environment].public.meta.title,
+        title: '毛豆网微课堂',
         suffix: 'Crowducate'
       }
     });
