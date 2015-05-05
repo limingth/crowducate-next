7c10ad2f236e2ca2eb32eb475d481da74ea88cae
Liu Jiyang <2411mail@gmail.com>
2015-05-05 16:59:38 +0800

    change title

--- both/router/meta.js
+++ both/router/meta.js
@@ -1,11 +1,9 @@
-Meteor.startup(function() {
-  if(Meteor.isClient) {
-    Meta.config({
-      options: {
-        //Meteor.settings[Meteor.settings.environment].public.meta.title,
-        title: '毛豆网微课堂',
-        suffix: 'Crowducate'
-      }
-    });
-  }
-});
+if(Meteor.isClient) {
+  Meta.config({
+    options: {
+      //Meteor.settings[Meteor.settings.environment].public.meta.title,
+      title: '毛豆网微课堂',
+      suffix: 'Crowducate'
+    }
+  });
+}
