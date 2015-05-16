7682cea617d2d314d6341a39efa0f4cdd040b5f6
Liu Jiyang <2411mail@gmail.com>
2015-05-16 23:15:13 +0800

    add auto save to summernote editor

--- client/templates/course/lesson/lesson.js
+++ client/templates/course/lesson/lesson.js
@@ -1,3 +1,17 @@
+// Autosave Timer
+var saveTimer = function(){
+  var timer;
+  this.set = function(saveFormCB) {
+    timer = Meteor.setTimeout(function() {
+      saveFormCB();
+    }, 3000);
+  };
+  this.clear = function() {
+    Meteor.clearInterval(timer);
+  };
+  return this;
+}();
+
 Template.lesson.helpers({
     'activeLesson': function () {
         // Get the lesson ID from reactive var
@@ -16,29 +30,41 @@ Template.lesson.helpers({
 });
 
 Template.lesson.events({
-    'click .lesson-text': function () {
-        // editing mode is true if text is clicked
-        editingLessonText.set(true);
+  'click .lesson-text': function () {
+    // editing mode is true if text is clicked
+    editingLessonText.set(true);
 
-        // in edit mode,
-        // add rich text editor
-        // to lesson text
-        if (editingLessonText.get() && (Meteor.userId() == "znNB43xrYAWRXKtWe" || Meteor.userId() == "feKnQQv5w4b6hkisX" || Meteor.userId() == "5Fp9XzMhyDEjfnipt" || Meteor.userId() == "6ryZES3ZPJeeYs8YY" || Meteor.userId() == "xgoJBP5wo2drCtKBb")) {
-            $('#rich-text-editor').summernote({
-                'height': 300,
-                'focus': true,
-                toolbar: [
-                    //[groupname, [button list]]
-                    ['style', ['bold', 'italic', 'underline', 'clear']],
-                    ['para', ['ul', 'ol']],
-                    ['insert', ['link','picture','video','hr']],
-                    ['view', ['fullscreen', 'codeview']],
-                    ['help', ['help']]
-                ]
-            });
+    // in edit mode,
+    // add rich text editor
+    // to lesson text
+    if (editingLessonText.get() && (Meteor.userId() == "znNB43xrYAWRXKtWe" || Meteor.userId() == "feKnQQv5w4b6hkisX" || Meteor.userId() == "5Fp9XzMhyDEjfnipt" || Meteor.userId() == "6ryZES3ZPJeeYs8YY" || Meteor.userId() == "xgoJBP5wo2drCtKBb")) {
+      $('#rich-text-editor').summernote({
+        'height': 300,
+        'focus': true,
+        toolbar: [
+          //[groupname, [button list]]
+          ['style', ['bold', 'italic', 'underline', 'clear']],
+          ['para', ['ul', 'ol']],
+          ['insert', ['link','picture','video','hr']],
+          ['view', ['fullscreen', 'codeview']],
+          ['help', ['help']]
+        ]
+      });
 
-            // Set the rich text editor code to lesson text
-            $('#rich-text-editor').code(this.text);
-        }
+      // Set the rich text editor code to lesson text
+      $('#rich-text-editor').code(this.text);
     }
+  },
+  'input [contenteditable=true]': function() {
+    // get the lesson ID
+    var lessonId = this._id;
+
+    saveTimer.set(function() {
+      // Get the HTML from rich text editor
+      var lessonText = $('#rich-text-editor').code();
+
+      // update the lesson in database
+      Lessons.update(lessonId, {$set: {'text': lessonText}});
+    });
+  }
 });
